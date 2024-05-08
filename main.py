8c1f1d8f2c876817c9a9bf5c04bf5f7c512e652d
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-05-08 08:28:39 -0500

    starting main script

---
+++ main.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import argparse
+import numpy as np
+from pathlib import Path
+from scipy.optimize import minimize
+from simsopt import make_optimizable
+from simsopt._core.derivative import Derivative
+from simsopt.solve import least_squares_mpi_solve
+from simsopt.mhd import Vmec, QuasisymmetryRatioResidual
+from simsopt.util import MpiPartition, proc0_print, comm_world
+from simsopt._core.finite_difference import MPIFiniteDifference
+from simsopt.field import BiotSavart, Current, coils_via_symmetries
+from simsopt.objectives import SquaredFlux, QuadraticPenalty, LeastSquaresProblem
+from simsopt.geo import (CurveLength, CurveCurveDistance, MeanSquaredCurvature, SurfaceRZFourier, LinkingNumber,
+                         LpCurveCurvature, ArclengthVariation, curves_to_vtk, create_equally_spaced_curves)
+mpi = MpiPartition()
+parent_path = str(Path(__file__).parent.resolve())
+os.chdir(parent_path)
+parser = argparse.ArgumentParser()
+parser.add_argument("--type", type=int, default=1)
+parser.add_argument("--ncoils", type=int, default=4)
+args = parser.parse_args()
+if   args.type == 1: QA_or_QH = 'QA'
+elif args.type == 2: QA_or_QH = 'QH'
+elif args.type == 3: QA_or_QH = 'QI'
+else: raise ValueError('Invalid type')
+ncoils = args.ncoils
+##########################################################################################
+############## Input parameters
+##########################################################################################
+optimize_stage_1_with_coils = False
+MAXITER_stage_1 = 10
+MAXITER_stage_2 = 200
+MAXITER_single_stage = 15
+MAXFEV_single_stage  = 20
+LENGTH_THRESHOLD = 4.5
+max_mode_array = [1]*2 + [2]*0 + [3]*0 + [4]*0 + [5]*0 + [6]*0
+nmodes_coils = 5
+aspect_ratio_target = 6
+JACOBIAN_THRESHOLD = 100
+aspect_ratio_weight = 3e-2
+mirror_weight = 1e+3
+quasisymmetry_weight = 1e+1
+coils_objective_weight = 1e+3
+weight_iota = 1e3
+min_iota = 0.41
+maxmodes_mpol_mapping = {1: 3, 2: 5, 3: 5, 4: 6, 5: 6, 6: 7}
+CC_THRESHOLD = 0.1
+CURVATURE_THRESHOLD = 10
+MSC_THRESHOLD = 10
+nphi_VMEC = 32
+ntheta_VMEC = 32
+ftol = 1e-3
+R0 = 1.0
+R1 = 0.58
+nquadpoints = 120
+diff_method = "forward"
+quasisymmetry_target_surfaces = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+finite_difference_abs_step = 1e-7
+finite_difference_rel_step = 1e-4
+LENGTH_CON_WEIGHT = 5.0e-2
+CC_WEIGHT = 3.6e+2
+CURVATURE_WEIGHT = 1e-2
+MSC_WEIGHT = 2.8e-5
+ARCLENGTH_WEIGHT = 1e-9
+vmec_input_filename = os.path.join(parent_path, 'input.'+ QA_or_QH)
+directory = f'optimization_{QA_or_QH}_ncoils{ncoils}'
+##########################################################################################
+##########################################################################################
+vmec_verbose = False
+# Create output directories
+this_path = os.path.join(parent_path, directory)
+os.makedirs(this_path, exist_ok=True)
+shutil.copyfile(os.path.join(parent_path, 'main.py'), os.path.join(this_path, 'main.py'))
+os.chdir(this_path)
+vmec_results_path = os.path.join(this_path, "vmec")
+coils_results_path = os.path.join(this_path, "coils")
+if comm_world.rank == 0:
+    os.makedirs(vmec_results_path, exist_ok=True)
+    os.makedirs(coils_results_path, exist_ok=True)
+##########################################################################################
+##########################################################################################
+# Stage 1
+proc0_print(f' Using vmec input file {vmec_input_filename}')
+vmec = Vmec(vmec_input_filename, mpi=mpi, verbose=vmec_verbose, nphi=nphi_VMEC, ntheta=ntheta_VMEC, range_surface='half period')
+surf = vmec.boundary
+nphi_big   = nphi_VMEC * 2 * surf.nfp + 1
+ntheta_big = ntheta_VMEC + 1
+quadpoints_theta = np.linspace(0, 1, ntheta_big)
+quadpoints_phi   = np.linspace(0, 1, nphi_big)
+surf_big = SurfaceRZFourier(dofs=surf.dofs, nfp=surf.nfp, mpol=surf.mpol, ntor=surf.ntor, quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta, stellsym=surf.stellsym)
+##########################################################################################
+##########################################################################################
+#Stage 2
+base_curves = create_equally_spaced_curves(ncoils, surf.nfp, stellsym=True, R0=R0, R1=R1, order=nmodes_coils, numquadpoints=nquadpoints)
+base_currents = [Current(1) * 1e5 for _ in range(ncoils)]
+base_currents[0].fix_all()
+coils = coils_via_symmetries(base_curves, base_currents, surf.nfp, stellsym=True)
+curves = [c.curve for c in coils]
+bs = BiotSavart(coils)
+##########################################################################################
+##########################################################################################
+# Save initial surface and coil data
+bs.set_points(surf.gamma().reshape((-1, 3)))
+Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+if comm_world.rank == 0:
+    curves_to_vtk(curves, os.path.join(coils_results_path, "curves_init"))
+    pointData = {"B.n/B": BdotN_surf[:, :, None]}
+    surf.to_vtk(os.path.join(coils_results_path, "surf_init"), extra_data=pointData)
+bs.set_points(surf_big.gamma().reshape((-1, 3)))
+Bbs = bs.B().reshape((nphi_big, ntheta_big, 3))
+BdotN_surf = np.sum(Bbs * surf_big.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+if comm_world.rank == 0:
+    pointData = {"B.n/B": BdotN_surf[:, :, None]}
+    surf_big.to_vtk(os.path.join(coils_results_path, "surf_init_big"), extra_data=pointData)
+bs.set_points(surf.gamma().reshape((-1, 3)))
+##########################################################################################
+##########################################################################################
+Jf = SquaredFlux(surf, bs, definition="local")
+Jls = [CurveLength(c) for c in base_curves]
+Jccdist = CurveCurveDistance(curves, CC_THRESHOLD, num_basecurves=len(curves))
+Jcs = [LpCurveCurvature(c, 2, CURVATURE_THRESHOLD) for i, c in enumerate(base_curves)]
+Jmscs = [MeanSquaredCurvature(c) for c in base_curves]
+Jals = [ArclengthVariation(c) for c in base_curves]
+J_CC = CC_WEIGHT * Jccdist
+J_CURVATURE = CURVATURE_WEIGHT * sum(Jcs)
+J_MSC = MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD, "max") for J in Jmscs)
+J_ALS = ARCLENGTH_WEIGHT * sum(Jals)
+J_LENGTH_PENALTY = LENGTH_CON_WEIGHT * sum(QuadraticPenalty(J, LENGTH_THRESHOLD, "max") for J in Jls)
+linkNum = LinkingNumber(curves)
+JF = Jf + J_CC + J_LENGTH_PENALTY + J_CURVATURE + J_MSC + J_ALS + linkNum
+##########################################################################################
+proc0_print('  Starting optimization')
+##########################################################################################
+# Initial stage 2 optimization
+##########################################################################################
+def fun_coils(dofss, info):
+    info['Nfeval'] += 1
+    JF.x = dofss
+    J = JF.J()
+    grad = JF.dJ()
+    if mpi.proc0_world:
+        jf = Jf.J()
+        Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+        BdotN = np.max(np.abs((np.sum(Bbs * surf.unitnormal(), axis=2)) / np.linalg.norm(Bbs, axis=2)))
+        outstr = f"fun_coils#{info['Nfeval']} - J={J:.1e}, Jf={jf:.1e}, max⟨B·n⟩/B={BdotN:.1e}"
+        outstr += f", ║∇J coils║={np.linalg.norm(JF.dJ()):.1e}, C-C-Sep={Jccdist.shortest_distance():.2f}"
+        cl_string = ", ".join([f"{j.J():.1f}" for j in Jls])
+        kap_string = ", ".join(f"{np.max(c.kappa()):.1f}" for c in base_curves)
+        msc_string = ", ".join(f"{j.J():.1f}" for j in Jmscs)
+        outstr += f" lengths=sum([{cl_string}])={sum(j.J() for j in Jls):.1f}, curv=[{kap_string}],msc=[{msc_string}]"
+        print(outstr)
+        # print(f"Currents: {[c.current.get_value() for c in coils]}")
+    return J, grad
+##########################################################################################
+# Single stage optimization
+##########################################################################################
+def fun(dofs, prob_jacobian=None, info={'Nfeval': 0}):
+    info['Nfeval'] += 1
+    J = prob.objective() + coils_objective_weight * JF.J()
+    if info['Nfeval'] > MAXFEV_single_stage and J < JACOBIAN_THRESHOLD:
+        return J, [0] * len(dofs)
+    JF.x = dofs[:-number_vmec_dofs]
+    prob.x = dofs[-number_vmec_dofs:]
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    os.chdir(vmec_results_path)
+    J_stage_1 = prob.objective()
+    J_stage_2 = coils_objective_weight * JF.J()
+    J = J_stage_1 + J_stage_2
+    if J > JACOBIAN_THRESHOLD or np.isnan(J):
+        proc0_print(f"Exception caught during function evaluation with J={J}. Returning J={JACOBIAN_THRESHOLD}")
+        J = JACOBIAN_THRESHOLD
+        grad_with_respect_to_surface = [0] * number_vmec_dofs
+        grad_with_respect_to_coils = [0] * len(JF.x)
+    else:
+        proc0_print(f"fun#{info['Nfeval']}: Objective function = {J}")
+        prob_dJ = prob_jacobian.jac(prob.x)
+        coils_dJ = JF.dJ()
+        n = surf.normal()
+        absn = np.linalg.norm(n, axis=2)
+        B = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+        dB_by_dX = bs.dB_by_dX().reshape((nphi_VMEC, ntheta_VMEC, 3, 3))
+        Bcoil = bs.B().reshape(n.shape)
+        unitn = n * (1./absn)[:, :, None]
+        Bcoil_n = np.sum(Bcoil*unitn, axis=2)
+        mod_Bcoil = np.linalg.norm(Bcoil, axis=2)
+        B_n = Bcoil_n
+        B_diff = Bcoil
+        B_N = np.sum(Bcoil * n, axis=2)
+        assert Jf.definition == "local"
+        dJdx = (B_n/mod_Bcoil**2)[:, :, None] * (np.sum(dB_by_dX*(n-B*(B_N/mod_Bcoil**2)[:, :, None])[:, :, None, :], axis=3))
+        dJdN = (B_n/mod_Bcoil**2)[:, :, None] * B_diff - 0.5 * (B_N**2/absn**3/mod_Bcoil**2)[:, :, None] * n
+        deriv = surf.dnormal_by_dcoeff_vjp(dJdN/(nphi_VMEC*ntheta_VMEC)) + surf.dgamma_by_dcoeff_vjp(dJdx/(nphi_VMEC*ntheta_VMEC))
+        mixed_dJ = Derivative({surf: deriv})(surf)
+        grad_with_respect_to_coils = coils_objective_weight * coils_dJ
+        grad_with_respect_to_surface = np.ravel(prob_dJ) + coils_objective_weight * mixed_dJ
+    grad = np.concatenate((grad_with_respect_to_coils, grad_with_respect_to_surface))
+    return J, grad
+##########################################################################################
+#############################################################
+## Perform optimization
+#############################################################
+##########################################################################################
+max_mode_previous = 0
+free_coil_dofs_all = JF.dofs_free_status
+for iteration, max_mode in enumerate(max_mode_array):
+    proc0_print(f'###############################################')
+    proc0_print(f'  Performing optimization for max_mode={max_mode}')
+    proc0_print(f'###############################################')
+    vmec.indata.mpol = maxmodes_mpol_mapping[max_mode]
+    vmec.indata.ntor = maxmodes_mpol_mapping[max_mode]
+    surf.fix_all()
+    surf.fixed_range(mmin=0, mmax=max_mode, nmin=-max_mode, nmax=max_mode, fixed=False)
+    surf.fix("rc(0,0)")
+    number_vmec_dofs = int(len(surf.x))
+    # Aspect ratio objective
+    def aspect_ratio_max_objective(vmec): return np.max((vmec.aspect()-aspect_ratio_target,0))
+    aspect_ratio_max_optimizable = make_optimizable(aspect_ratio_max_objective, vmec)
+    objective_tuple = [(aspect_ratio_max_optimizable.J, 0, aspect_ratio_weight)]
+    # Iota objective
+    def iota_min_objective(vmec): return np.min((np.min(np.abs(vmec.wout.iotaf))-min_iota,0))
+    iota_min_optimizable = make_optimizable(iota_min_objective, vmec)
+    objective_tuple.append((iota_min_optimizable.J, 0, weight_iota))
+    # Quasisymmetry objective
+    qs = QuasisymmetryRatioResidual(vmec, quasisymmetry_target_surfaces, helicity_m=1, helicity_n=-1 if QA_or_QH == 'QH' else 0)
+    objective_tuple.append((qs.residuals, 0, quasisymmetry_weight))
+    # Put all together
+    prob = LeastSquaresProblem.from_tuples(objective_tuple)
+    dofs = np.concatenate((JF.x, vmec.x))
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    Jf = SquaredFlux(surf, bs, definition="local")
+    proc0_print(f"Aspect ratio before optimization: {vmec.aspect()}")
+    proc0_print(f"Mean iota before optimization: {vmec.mean_iota()}")
+    proc0_print(f"Quasisymmetry objective before optimization: {qs.total()}")
+    proc0_print(f"Magnetic well before optimization: {vmec.vacuum_well()}")
+    proc0_print(f"Squared flux before optimization: {Jf.J()}")
+    ### Stage 2 optimization
+    proc0_print(f'  Performing stage 2 optimization with ~{MAXITER_stage_2} iterations')
+    if comm_world.rank == 0:
+        JF.full_unfix(free_coil_dofs_all)
+        res = minimize(fun_coils, dofs[:-number_vmec_dofs], jac=True, args=({'Nfeval': 0}), method='L-BFGS-B', options={'maxiter': MAXITER_stage_2, 'maxcor': 300}, tol=1e-9)
+        dofs[:-number_vmec_dofs] = res.x
+    mpi.comm_world.Bcast(dofs, root=0)
+    JF.x = dofs[:-number_vmec_dofs]
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    ## Stage 1 optimization with coils
+    if optimize_stage_1_with_coils:
+        def JF_objective(vmec):
+            bs.set_points(vmec.boundary.gamma().reshape((-1, 3)))
+            return JF.J()
+        JF_objective_optimizable = make_optimizable(JF_objective, vmec)
+        Jf_residual = JF_objective_optimizable.J()
+        prob_residual = prob.objective()
+        new_Jf_weight = (prob_residual/Jf_residual)**2
+        objective_tuples_with_coils = tuple(objective_tuple)+tuple([(JF_objective_optimizable.J, 0, new_Jf_weight)])
+        prob_with_coils = LeastSquaresProblem.from_tuples(objective_tuples_with_coils)
+        proc0_print(f'  Performing stage 1 optimization with coils with ~{MAXITER_stage_1} iterations')
+        free_coil_dofs_all = JF.dofs_free_status
+        JF.fix_all()
+        least_squares_mpi_solve(prob_with_coils, mpi, grad=True, rel_step=1e-5, abs_step=1e-7, max_nfev=MAXITER_stage_1, ftol=1e-04, xtol=1e-04, gtol=1e-04)
+        JF.full_unfix(free_coil_dofs_all)
+    ## Broadcast dofs and save surfs/coils
+    mpi.comm_world.Bcast(dofs, root=0)
+    JF.x = dofs[:-number_vmec_dofs]
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+    BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+    if comm_world.rank == 0:
+        curves_to_vtk(base_curves, os.path.join(coils_results_path, f"base_curves_after_stage2_maxmode{max_mode}"))
+        curves_to_vtk(curves, os.path.join(coils_results_path, f"curves_after_stage2_maxmode{max_mode}"))
+        pointData = {"B.n/B": BdotN_surf[:, :, None]}
+        surf.to_vtk(os.path.join(coils_results_path, f"surf_after_stage2_maxmode{max_mode}"), extra_data=pointData)
+    bs.set_points(surf_big.gamma().reshape((-1, 3)))
+    Bbs = bs.B().reshape((nphi_big, ntheta_big, 3))
+    BdotN_surf = np.sum(Bbs * surf_big.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+    if comm_world.rank == 0:
+        pointData = {"B.n/B": BdotN_surf[:, :, None]}
+        surf_big.to_vtk(os.path.join(coils_results_path, f"surf_big_after_stage2_maxmode{max_mode}"), extra_data=pointData)
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    ## Single stage optimization
+    proc0_print(f'  Performing single stage optimization with ~{MAXITER_single_stage} iterations')
+    x0 = np.copy(np.concatenate((JF.x, vmec.x)))
+    dofs = np.concatenate((JF.x, vmec.x))
+    with MPIFiniteDifference(prob.objective, mpi, diff_method=diff_method, abs_step=finite_difference_abs_step, rel_step=finite_difference_rel_step) as prob_jacobian:
+        if mpi.proc0_world:
+            res = minimize(fun, dofs, args=(prob_jacobian, {'Nfeval': 0}), jac=True, method='BFGS', options={'maxiter': MAXITER_single_stage, 'maxfev': MAXFEV_single_stage, 'gtol': ftol, 'ftol': ftol}, tol=ftol)
+    ## Broadcast dofs and save surfs/coils
+    mpi.comm_world.Bcast(dofs, root=0)
+    Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+    BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+    if comm_world.rank == 0:
+        curves_to_vtk(base_curves, os.path.join(coils_results_path, f"base_curves_opt_maxmode{max_mode}"))
+        curves_to_vtk(curves, os.path.join(coils_results_path, f"curves_opt_maxmode{max_mode}"))
+        pointData = {"B.n/B": BdotN_surf[:, :, None]}
+        surf.to_vtk(os.path.join(coils_results_path, f"surf_opt_maxmode{max_mode}"), extra_data=pointData)
+    bs.set_points(surf_big.gamma().reshape((-1, 3)))
+    Bbs = bs.B().reshape((nphi_big, ntheta_big, 3))
+    BdotN_surf = np.sum(Bbs * surf_big.unitnormal(), axis=2) / np.linalg.norm(Bbs, axis=2)
+    if comm_world.rank == 0:
+        pointData = {"B.n/B": BdotN_surf[:, :, None]}
+        surf_big.to_vtk(os.path.join(coils_results_path, f"surf_big_opt_maxmode{max_mode}"), extra_data=pointData)
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+    bs.save(os.path.join(coils_results_path, f"biot_savart_maxmode{max_mode}.json"))
+    vmec.write_input(os.path.join(this_path, f'input.maxmode{max_mode}'))
+##########################################################################################
+############## Save final results
+##########################################################################################
+bs.save(os.path.join(coils_results_path, "biot_savart_opt.json"))
+vmec.write_input(os.path.join(this_path, 'input.final'))
+proc0_print(f"Aspect ratio after optimization: {vmec.aspect()}")
+proc0_print(f"Mean iota after optimization: {vmec.mean_iota()}")
+proc0_print(f"Quasisymmetry objective after optimization: {qs.total()}")
+proc0_print(f"Magnetic well after optimization: {vmec.vacuum_well()}")
+proc0_print(f"Squared flux after optimization: {Jf.J()}")
+BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
+BdotN = np.mean(np.abs(BdotN_surf))
+BdotNmax = np.max(np.abs(BdotN_surf))
+outstr = f"Coil parameters: ⟨B·n⟩={BdotN:.1e}, B·n max={BdotNmax:.1e}"
+outstr += f", ║∇J coils║={np.linalg.norm(JF.dJ()):.1e}, C-C-Sep={Jccdist.shortest_distance():.2f}"
+cl_string = ", ".join([f"{j.J():.1f}" for j in Jls])
+kap_string = ", ".join(f"{np.max(c.kappa()):.1f}" for c in base_curves)
+msc_string = ", ".join(f"{j.J():.1f}" for j in Jmscs)
+outstr += f" lengths=sum([{cl_string}])={sum(j.J() for j in Jls):.1f}, curv=[{kap_string}], msc=[{msc_string}]"
+proc0_print(outstr)
